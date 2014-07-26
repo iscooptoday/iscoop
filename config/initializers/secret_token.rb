301b2eca706cdf0e058fa568ad4c0e32928127f0
iscoop <mkromah@hotmail.co.uk>
2014-07-26 17:32:32 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iscoop::Application.config.secret_key_base = 'afd822ebc88c59b007a7cf1923450c6534efd840a542ee04b5dc4d29145a14ce610b21aebcebdce2e82d3b06f7b63f1e91a3ad088088673b943ab2f1bd765827'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Iscoop::Application.config.session_store :cookie_store, key: '_iscoop_session'
